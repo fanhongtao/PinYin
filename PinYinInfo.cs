e45120aec507dffe1930d93849a526124b984c7a
Fan Hongtao <fanhongtao@gmail.com>
2016-05-04 21:04:42 +0800

    自动添加 base 目录下的XML文件（暂不支持多音字）

--- PinYinInfo.cs
+++ PinYinInfo.cs
@@ -8,6 +8,7 @@
  */
 using System;
 using System.Collections;
+using System.IO;
 using System.Xml;
 
 namespace PinYin
@@ -32,15 +33,28 @@ namespace PinYin
 		
 		public void load()
 		{
-			XmlDocument doc = new XmlDocument();
-			doc.Load("base.xml");    //加载Xml文件
-			XmlElement rootElem = doc.DocumentElement;   //获取根节点
-			XmlNodeList personNodes = rootElem.GetElementsByTagName("item"); //获取person子节点集合
-			foreach (XmlNode node in personNodes)
-			{
-				string ch = ((XmlElement)node).GetAttribute("ch");   //获取ch属性值
-				string py = ((XmlElement)node).GetAttribute("py");   //获取py属性值
-				hashTable.Add(ch, py);
+			loadBase();
+		}
+		
+		// 加载  Base 目录
+		private void loadBase()
+		{
+			DirectoryInfo dir = new DirectoryInfo("pinyin/base");
+			FileInfo[] files = dir.GetFiles("*.xml");
+			foreach (FileInfo file in files) {
+				XmlDocument doc = new XmlDocument();
+				Logger.info("Load " + file.FullName);
+				doc.Load(file.FullName);    //加载Xml文件
+				XmlElement rootElem = doc.DocumentElement;   //获取根节点
+				XmlNodeList personNodes = rootElem.GetElementsByTagName("char"); //获取char子节点集合
+				foreach (XmlNode node in personNodes)
+				{
+					string hz = ((XmlElement)node).GetAttribute("hz");   //获取hz属性值
+					string py = ((XmlElement)node).GetAttribute("py");   //获取py属性值
+					if (!hashTable.Contains(hz)) {
+						hashTable.Add(hz, py);
+					}
+				}
 			}
 		}
 		
