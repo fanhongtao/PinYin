a48f007f8285b211cb5b823c2c5fa7024ca6290d
Fan Hongtao <fanhongtao@gmail.com>
2016-05-05 21:32:54 +0800

    读取多音字（初稿）

--- PinYinInfo.cs
+++ PinYinInfo.cs
@@ -8,6 +8,7 @@
  */
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 
@@ -18,6 +19,37 @@ namespace PinYin
 	/// </summary>
 	public sealed class PinYinInfo
 	{
+		string CHAR = "char";
+		string PHARSE = "pharse";
+		string HZ = "hz";
+		string PY = "py";
+		string MULTI = "multi";
+		string MAIN = "main";
+		string TRANS = "trans";
+		
+		class PhraseInfo
+		{
+			public string hanzi { get; set; }   // 词组对应的汉字
+			public string pinyin { get; set; }  // 词组对应的拼音
+		}
+		
+		class CharInfo
+		{
+			public string hanzi { get; set; }  // 汉字
+			public bool   multi { get; set; }  // 是否是多音字
+			public bool   trans { get; set; }  // 是否是繁体字（异休字）
+			public string info { get; set; }   // 注释信息
+			public List<String> pinyins;       // 汉字的发音
+			public List<PhraseInfo> pharses;   // 汉字对应的词组
+			public CharInfo()
+			{
+				pinyins = new List<string>();
+				pharses = null;
+				multi = false;
+				trans = false;
+			}
+		}
+		
 		private static PinYinInfo instance = new PinYinInfo();
 		private Hashtable hashTable = new Hashtable();
 		
@@ -46,15 +78,64 @@ namespace PinYin
 				Logger.info("Load " + file.FullName);
 				doc.Load(file.FullName);    //加载Xml文件
 				XmlElement rootElem = doc.DocumentElement;   //获取根节点
-				XmlNodeList personNodes = rootElem.GetElementsByTagName("char"); //获取char子节点集合
-				foreach (XmlNode node in personNodes)
+				XmlNodeList charNodes = rootElem.GetElementsByTagName(CHAR); //获取char子节点集合
+				foreach (XmlNode node in charNodes)
 				{
-					string hz = ((XmlElement)node).GetAttribute("hz");   //获取hz属性值
-					string py = ((XmlElement)node).GetAttribute("py");   //获取py属性值
-					if (!hashTable.Contains(hz)) {
-						hashTable.Add(hz, py);
+					string hz = ((XmlElement)node).GetAttribute(HZ);   //获取hz属性值
+					string py = ((XmlElement)node).GetAttribute(PY);   //获取py属性值
+					CharInfo charInfo;
+					
+					// 不是多音字，直接构造一个CharInfo添加到 hashTable
+					if (!((XmlElement)node).HasAttribute(MULTI)) {
+						charInfo = new CharInfo();
+						charInfo.hanzi = hz;
+						charInfo.pinyins.Add(py);
+						charInfo.multi = false;
+						if (((XmlElement)node).HasAttribute(TRANS)) {
+							charInfo.trans = StringTools.ToBoolean(((XmlElement)node).GetAttribute(TRANS));
+						}
+						hashTable.Add(hz, charInfo);
+						continue;
 					}
+					
+					// 是多音字
+					if (!hashTable.Contains(hz)) {
+						charInfo = new CharInfo();
+						charInfo.hanzi = hz;
+						charInfo.multi = true;
+						if (((XmlElement)node).HasAttribute(TRANS)) {
+							charInfo.trans = StringTools.ToBoolean(((XmlElement)node).GetAttribute(TRANS));
+						}
+						hashTable.Add(hz, charInfo);
+					} else {
+						charInfo = (CharInfo)hashTable[hz];
+					}
+					
+					// 添加多音字的拼音
+					if (((XmlElement)node).HasAttribute(MAIN)
+					    && StringTools.ToBoolean(((XmlElement)node).GetAttribute(MAIN))) {
+						charInfo.pinyins.Insert(0, py);
+					} else {
+						charInfo.pinyins.Add(py);
+					}
+					
+					// 添加多音字可能的词组
+					XmlNodeList pharseNodes = ((XmlElement)node).GetElementsByTagName(PHARSE); //获取pharse子节点集合
+					addPharse(charInfo, pharseNodes);
 				}
+			}
+		}
+		
+		private void addPharse(CharInfo charInfo, XmlNodeList pharseNodes)
+		{
+			foreach (XmlNode node in pharseNodes) {
+				string hz = ((XmlElement)node).GetAttribute(HZ);   //获取hz属性值
+				string py = ((XmlElement)node).GetAttribute(PY);   //获取py属性值
+				
+				PhraseInfo pharseInfo = new PhraseInfo();
+				pharseInfo.hanzi = hz;
+				pharseInfo.pinyin = py;
+				charInfo.pharses.Add(pharseInfo);
 			}
 		}
 		
@@ -65,7 +146,8 @@ namespace PinYin
 		
 		public string getPinYin(string ch)
 		{
-			return (string)hashTable[ch];
+			CharInfo charInfo = (CharInfo) hashTable[ch];
+			return charInfo.pinyins[0];
 		}
 	}
 }
---
+++ StringTools.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fht
+ * Date: 2016/5/5
+ * Time: 15:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PinYin
+{
+	/// <summary>
+	/// Description of StringTools.
+	/// </summary>
+	public static class StringTools
+	{
+		public static bool ToBoolean(this string value)
+		{
+			switch (value.ToLower())
+			{
+				case  "true":
+					return true;
+				case "t":
+					return true;
+				case "1":
+					return true;
+				case "0":
+					return false;
+				case "false":
+					return false;
+				case "f":
+					return false;
+				default:
+					throw new InvalidCastException("Can't convert [" + value + "] to a bool!");
+			}
+		}
+	}
+}
