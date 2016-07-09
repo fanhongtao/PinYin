72f2a2a835be5feb35a12f4ae9fa46862ef5ffd9
Fan Hongtao <fanhongtao@gmail.com>
2016-07-09 16:31:06 +0800

    调整代码结构

--- ConvertForm.cs
+++ ConvertForm.cs
@@ -33,54 +33,28 @@ namespace PinYin
 		{
 			outputText.ResetText();
 			
-			PinYinInfo baseInfo = PinYinInfo.Instance;
-			ExtraInfo extraInfo = ExtraInfo.Instance;
 			string[] lines = inputText.Lines;
 			foreach (string line in lines) {
 				for (int i = 0; i < line.Length; i++) {
 					string ch = line.Substring(i, 1);
+					
 					int matchLen = matchExtra(line, i, ch);
 					if (matchLen > 0) {
 						i = i + (matchLen - 1); // 跳过已经匹配的字
 						continue;
 					}
 					
-					CharInfo charInfo = baseInfo.getCharInfo(ch);
-					if (charInfo == null) {
-						if (StringTools.IsChineseCharacter(ch)) {
-							// 找不到相应的字，给出相应的提示
-							Logger.error("Can't find character [" + ch + "]");
-						}
-						outputText.AppendText(ch);
+					matchLen = matchBase(line, i, ch);
+					if (matchLen > 0) {
+						i = i + (matchLen - 1); // 跳过已经匹配的字
 						continue;
 					}
 					
-					bool matchPhrase = false;  // 是否和某个词组匹配成功
-					foreach (PhraseInfo phrase in charInfo.phrases) {
-						int len = phrase.hanzi.Length;
-						if (i + len > line.Length) { // 剩余字数少于词组的字数，直接跳过该词
-							continue;
-						}
-						string temp = line.Substring(i, len);
-						if (temp.Equals(phrase.hanzi)) {
-							if (phraseCheckBox.Checked) {
-								string pinyin = string.Join(",", phrase.pinyin);
-								Logger.debug("Match [" + phrase.hanzi + "] - (" + pinyin + ")");
-							}
-							for (int idx = 0; idx < len; idx++) {
-								outputText.AppendText(phrase.hanzi.Substring(idx, 1));
-								outputText.AppendText("(" + phrase.pinyin[idx] + ")");
-							}
-							i = i + (len - 1); // 跳过已经匹配的字
-							matchPhrase = true;
-							break;
-						}
+					if (StringTools.IsChineseCharacter(ch)) {
+						// 找不到相应的字，给出相应的提示
+						Logger.error("Can't find character [" + ch + "]");
 					}
-					
-					if (!matchPhrase) {
-						outputText.AppendText(ch);
-						outputText.AppendText("(" + charInfo.pinyins[0] + ")");
-					}
+					outputText.AppendText(ch);
 				}
 				outputText.AppendText("\n");
 			}
@@ -125,6 +99,46 @@ namespace PinYin
 			}
 			
 			return 0;
+		}
+		
+		/// <summary>
+		/// 尝试根据 base 目录的内容来匹配
+		/// </summary>
+		/// <param name="line">当前要处理的行</param>
+		/// <param name="i">当前要处理的字在该行中的位置</param>
+		/// <param name="ch">当前要处理的字</param>
+		/// <returns>匹配的字的个数（0表示没有匹配成功）</returns>
+		private int matchBase(string line, int i, string ch)
+		{
+			PinYinInfo baseInfo = PinYinInfo.Instance;
+			CharInfo charInfo = baseInfo.getCharInfo(ch);
+			if (charInfo == null) {
+				return 0;
+			}
+			
+			foreach (PhraseInfo phrase in charInfo.phrases) {
+				int len = phrase.hanzi.Length;
+				if (i + len > line.Length) { // 剩余字数少于词组的字数，直接跳过该词
+					continue;
+				}
+				
+				string temp = line.Substring(i, len);
+				if (temp.Equals(phrase.hanzi)) {
+					if (phraseCheckBox.Checked) {
+						string pinyin = string.Join(",", phrase.pinyin);
+						Logger.debug("Match [" + phrase.hanzi + "] - (" + pinyin + ")");
+					}
+					for (int idx = 0; idx < len; idx++) {
+						outputText.AppendText(phrase.hanzi.Substring(idx, 1));
+						outputText.AppendText("(" + phrase.pinyin[idx] + ")");
+					}
+					return len;
+				}
+			}
+			
+			outputText.AppendText(ch);
+			outputText.AppendText("(" + charInfo.pinyins[0] + ")");
+			return 1;
 		}
 	}
 }
